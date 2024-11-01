4b3bdfd91a16d2609e3795b4a6d5cab3ca3aec92
Viktoriia Herchanivska <viktoriia.herchanivska@gmail.com>
2024-11-01 15:56:03 +0200

    update test

--- service/src/main/java/greencity/service/ToDoListItemServiceImpl.java
+++ service/src/main/java/greencity/service/ToDoListItemServiceImpl.java
@@ -130,12 +130,12 @@ public class ToDoListItemServiceImpl implements ToDoListItemService {
      */
     @Override
     public PageableAdvancedDto<ToDoListItemManagementDto> findToDoListItemsForManagementByPage(
-            Pageable pageable) {
+        Pageable pageable) {
         Page<ToDoListItem> toDoListItems = toDoListItemRepo.findAll(pageable);
         List<ToDoListItemManagementDto> toDoListItemManagementDtos =
-                toDoListItems.getContent().stream()
-                        .map(item -> modelMapper.map(item, ToDoListItemManagementDto.class))
-                        .collect(Collectors.toList());
+            toDoListItems.getContent().stream()
+                .map(item -> modelMapper.map(item, ToDoListItemManagementDto.class))
+                .collect(Collectors.toList());
         return getPagebleAdvancedDto(toDoListItemManagementDtos, toDoListItems);
     }
 
@@ -155,8 +155,8 @@ public class ToDoListItemServiceImpl implements ToDoListItemService {
     public PageableAdvancedDto<ToDoListItemManagementDto> searchBy(Pageable paging, String query) {
         Page<ToDoListItem> toDoListItems = toDoListItemRepo.searchBy(paging, query);
         List<ToDoListItemManagementDto> toDoListItemManagementDtos = toDoListItems.stream()
-                .map(item -> modelMapper.map(item, ToDoListItemManagementDto.class))
-                .collect(Collectors.toList());
+            .map(item -> modelMapper.map(item, ToDoListItemManagementDto.class))
+            .collect(Collectors.toList());
         return getPagebleAdvancedDto(toDoListItemManagementDtos, toDoListItems);
     }
 
@@ -165,7 +165,7 @@ public class ToDoListItemServiceImpl implements ToDoListItemService {
      */
     @Override
     public PageableAdvancedDto<ToDoListItemManagementDto> getFilteredDataForManagementByPage(Pageable pageable,
-                                                                                             ToDoListItemViewDto dto) {
+        ToDoListItemViewDto dto) {
         Page<ToDoListItem> pages = toDoListItemRepo.findAll(getSpecification(dto), pageable);
         return getPagesFilteredPages(pages);
     }
@@ -176,8 +176,8 @@ public class ToDoListItemServiceImpl implements ToDoListItemService {
     @Transactional
     @Override
     public List<UserToDoListItemResponseDto> saveUserToDoListItems(Long userId, Long habitId,
-                                                                   List<ToDoListItemRequestDto> dtoList,
-                                                                   String language) {
+        List<ToDoListItemRequestDto> dtoList,
+        String language) {
         if (dtoList != null) {
             Optional<HabitAssign> habitAssign = habitAssignRepo.findByHabitIdAndUserId(habitId, userId);
             if (habitAssign.isPresent()) {
@@ -210,10 +210,10 @@ public class ToDoListItemServiceImpl implements ToDoListItemService {
      */
     @Override
     public List<UserToDoListItemResponseDto> getUserToDoListByHabitAssignId(Long userId, Long habitAssignId,
-                                                                            String language) {
+        String language) {
         HabitAssign habitAssign = habitAssignRepo.findById(habitAssignId)
-                .orElseThrow(() -> new NotFoundException(
-                        ErrorMessage.HABIT_ASSIGN_NOT_FOUND_BY_ID + habitAssignId));
+            .orElseThrow(() -> new NotFoundException(
+                ErrorMessage.HABIT_ASSIGN_NOT_FOUND_BY_ID + habitAssignId));
 
         if (!habitAssign.getUser().getId().equals(userId)) {
             throw new UserHasNoPermissionToAccessException(ErrorMessage.USER_HAS_NO_PERMISSION);
@@ -227,12 +227,12 @@ public class ToDoListItemServiceImpl implements ToDoListItemService {
     @Transactional
     @Override
     public List<UserToDoListItemResponseDto> getUserToDoListItemsByHabitAssignIdAndStatusInProgress(
-            Long habitAssignId, String language) {
+        Long habitAssignId, String language) {
         return userToDoListItemRepo
-                .findUserToDoListItemsByHabitAssignIdAndStatusInProgress(habitAssignId)
-                .stream()
-                .map(userToDoListItem -> converterUserToDoListItemResponseDto(userToDoListItem, language))
-                .collect(Collectors.toList());
+            .findUserToDoListItemsByHabitAssignIdAndStatusInProgress(habitAssignId)
+            .stream()
+            .map(userToDoListItem -> converterUserToDoListItemResponseDto(userToDoListItem, language))
+            .collect(Collectors.toList());
     }
 
     /**
@@ -241,7 +241,7 @@ public class ToDoListItemServiceImpl implements ToDoListItemService {
     @Override
     public void deleteUserToDoListItemByItemIdAndUserIdAndHabitId(Long itemId, Long userId, Long habitId) {
         userToDoListItemRepo.deleteByToDoListItemIdAndHabitAssignId(itemId,
-                getHabitAssignByHabitIdAndUserIdAndSuspendedFalse(userId, habitId).getId());
+            getHabitAssignByHabitIdAndUserIdAndSuspendedFalse(userId, habitId).getId());
     }
 
     /**
@@ -256,10 +256,10 @@ public class ToDoListItemServiceImpl implements ToDoListItemService {
             changeStatusToDone(userToDoListItem);
         } else {
             throw new UserToDoListItemStatusNotUpdatedException(
-                    ErrorMessage.USER_TO_DO_LIST_ITEMS_STATUS_IS_ALREADY_DONE + userToDoListItem.getId());
+                ErrorMessage.USER_TO_DO_LIST_ITEMS_STATUS_IS_ALREADY_DONE + userToDoListItem.getId());
         }
         UserToDoListItemResponseDto updatedUserToDoListItem =
-                modelMapper.map(userToDoListItem, UserToDoListItemResponseDto.class);
+            modelMapper.map(userToDoListItem, UserToDoListItemResponseDto.class);
         setTextForUserToDoListItem(updatedUserToDoListItem, language);
         return updatedUserToDoListItem;
     }
@@ -270,12 +270,12 @@ public class ToDoListItemServiceImpl implements ToDoListItemService {
     @Transactional
     @Override
     public List<UserToDoListItemResponseDto> updateUserToDoListItemStatus(Long userId,
-                                                                          Long userToDoListItemId,
-                                                                          String language,
-                                                                          String status) {
+        Long userToDoListItemId,
+        String language,
+        String status) {
         String statusUpperCase = status.toUpperCase();
         List<UserToDoListItem> userToDoListItems =
-                userToDoListItemRepo.getAllByUserToDoListIdAndUserId(userToDoListItemId, userId);
+            userToDoListItemRepo.getAllByUserToDoListIdAndUserId(userToDoListItemId, userId);
         if (userToDoListItems == null || userToDoListItems.isEmpty()) {
             throw new NotFoundException(ErrorMessage.USER_TO_DO_LIST_ITEM_NOT_FOUND_BY_USER_ID);
         }
@@ -285,9 +285,9 @@ public class ToDoListItemServiceImpl implements ToDoListItemService {
         userToDoListItems.forEach(u -> u.setStatus(ToDoListItemStatus.valueOf(statusUpperCase)));
         userToDoListItemRepo.saveAll(userToDoListItems);
         List<UserToDoListItemResponseDto> userToDoListItemResponseDtoList =
-                userToDoListItems.stream()
-                        .map(u -> modelMapper.map(u, UserToDoListItemResponseDto.class))
-                        .collect(Collectors.toList());
+            userToDoListItems.stream()
+                .map(u -> modelMapper.map(u, UserToDoListItemResponseDto.class))
+                .collect(Collectors.toList());
         userToDoListItemResponseDtoList.forEach(u -> setTextForUserToDoListItem(u, language));
         return userToDoListItemResponseDtoList;
     }
@@ -301,8 +301,8 @@ public class ToDoListItemServiceImpl implements ToDoListItemService {
     @Override
     public List<Long> deleteUserToDoListItems(String ids) {
         List<Long> arrayId = Arrays.stream(ids.split(","))
-                .map(Long::valueOf)
-                .toList();
+            .map(Long::valueOf)
+            .toList();
 
         List<Long> deleted = new ArrayList<>();
         for (Long id : arrayId) {
@@ -314,7 +314,7 @@ public class ToDoListItemServiceImpl implements ToDoListItemService {
     @Override
     public List<ToDoListItemManagementDto> getToDoListByHabitId(Long habitId) {
         List<Long> idList =
-                toDoListItemRepo.getAllToDoListItemIdByHabitIdISContained(habitId);
+            toDoListItemRepo.getAllToDoListItemIdByHabitIdISContained(habitId);
         List<ToDoListItem> toDoListItems;
         if (!idList.isEmpty()) {
             toDoListItems = toDoListItemRepo.getToDoListByListOfId(idList);
@@ -323,33 +323,33 @@ public class ToDoListItemServiceImpl implements ToDoListItemService {
         }
 
         return toDoListItems.stream()
-                .map(listItem -> modelMapper.map(listItem, ToDoListItemManagementDto.class))
-                .collect(Collectors.toList());
+            .map(listItem -> modelMapper.map(listItem, ToDoListItemManagementDto.class))
+            .collect(Collectors.toList());
     }
 
     @Override
     public PageableAdvancedDto<ToDoListItemManagementDto> findAllToDoListItemsForManagementPageNotContained(
-            Long habitId,
-            Pageable pageable) {
+        Long habitId,
+        Pageable pageable) {
         List<Long> items =
-                toDoListItemRepo.getAllToDoListItemsByHabitIdNotContained(habitId);
+            toDoListItemRepo.getAllToDoListItemsByHabitIdNotContained(habitId);
         Page<ToDoListItem> toDoListItems =
-                toDoListItemRepo
-                        .getToDoListByListOfIdPageable(items, pageable);
+            toDoListItemRepo
+                .getToDoListByListOfIdPageable(items, pageable);
         List<ToDoListItemManagementDto> toDoListItemManagementDtos =
-                toDoListItems.getContent().stream()
-                        .map(item -> modelMapper.map(item, ToDoListItemManagementDto.class))
-                        .collect(Collectors.toList());
+            toDoListItems.getContent().stream()
+                .map(item -> modelMapper.map(item, ToDoListItemManagementDto.class))
+                .collect(Collectors.toList());
         return getPagebleAdvancedDto(toDoListItemManagementDtos, toDoListItems);
     }
 
     @Override
     public List<ToDoListItemDto> findInProgressByUserIdAndLanguageCode(Long userId, String code) {
         List<ToDoListItemDto> toDoListItemDtos = toDoListItemRepo
-                .findInProgressByUserIdAndLanguageCode(userId, code)
-                .stream()
-                .map(toDoListItemTranslation -> modelMapper.map(toDoListItemTranslation, ToDoListItemDto.class))
-                .collect(Collectors.toList());
+            .findInProgressByUserIdAndLanguageCode(userId, code)
+            .stream()
+            .map(toDoListItemTranslation -> modelMapper.map(toDoListItemTranslation, ToDoListItemDto.class))
+            .collect(Collectors.toList());
         toDoListItemDtos.forEach(x -> x.setStatus(ToDoListItemStatus.INPROGRESS.toString()));
         return toDoListItemDtos;
     }
@@ -468,7 +468,6 @@ public class ToDoListItemServiceImpl implements ToDoListItemService {
         userToDoListItemRepo.saveAll(habitAssign.getUserToDoListItems());
     }
 
-
     private List<UserToDoListItemResponseDto> getAllUserToDoListItems(HabitAssign habitAssign) {
         return userToDoListItemRepo
             .findAllByHabitAssingId(habitAssign.getId())
@@ -487,7 +486,6 @@ public class ToDoListItemServiceImpl implements ToDoListItemService {
             toDoListItemTranslationRepo.findByLangAndUserToDoListItemId(language, dto.getId()).getContent();
         dto.setText(text);
     }
-
 
     /**
      * Method converts UserToDoListItem to UserToDoListItemResponseDto and sets text
@@ -512,8 +510,6 @@ public class ToDoListItemServiceImpl implements ToDoListItemService {
         throw new NotFoundException(ErrorMessage.HABIT_ASSIGN_NOT_FOUND_BY_ID);
     }
 
-
-
     private boolean isActive(UserToDoListItem userToDoListItem) {
         try {
             if (ToDoListItemStatus.ACTIVE.equals(userToDoListItem.getStatus())) {
@@ -531,7 +527,6 @@ public class ToDoListItemServiceImpl implements ToDoListItemService {
         userToDoListItem.setDateCompleted(LocalDateTime.now());
         userToDoListItemRepo.save(userToDoListItem);
     }
-
 
     /**
      * Method for deleting user to-do list item by id.
