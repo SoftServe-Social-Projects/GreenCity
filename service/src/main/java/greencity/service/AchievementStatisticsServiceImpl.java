6c969aa7064d770fb70d3e521f4498a99bbdcc45
Maryna <m51571750@gmail.com>
2024-11-08 14:24:42 +0200

    Added Achievements Statistics Section in GreenCity Admin Panel

---
+++ service/src/main/java/greencity/service/AchievementStatisticsServiceImpl.java
@@ -0,0 +1,30 @@
+package greencity.service;
+
+import greencity.dto.achievement.StatisticsDto;
+import greencity.repository.AchievementRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@EnableCaching
+public class AchievementStatisticsServiceImpl implements AchievementStatisticsService {
+    private final AchievementRepo achievementRepo;
+
+    @Override
+    public List<StatisticsDto> statisticsUsersWithAchievements() {
+        return achievementRepo.getStatisticsUsersWithAchievements();
+    }
+
+    @Override
+    public List<StatisticsDto> statisticsUsersWithAchievementsCategory() {
+        return achievementRepo.getStatisticsUsersWithAchievementsCategory();
+    }
+
+    @Override
+    public List<StatisticsDto> statisticsUsersActivity() {
+        return achievementRepo.getUserActivityStatistics();
+    }
+}
