584d8016a23e27e85a8674ace41d45bfa5f351cd
Maryna <m51571750@gmail.com>
2024-12-03 12:03:10 +0200

    Fixed style

--- service-api/src/main/java/greencity/constant/OpenAIRequest.java
+++ service-api/src/main/java/greencity/constant/OpenAIRequest.java
@@ -5,7 +5,7 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class OpenAIRequest {
     public static final String FORECAST =
-        "respond in a personalized manner, concisely, and accurately. Use numbers to present approximate data. " +
-            "Provide a forecast of this person's impact on the global environment, considering their habits " +
-            "over the specified number of days:";
+        "respond in a personalized manner, concisely, and accurately. Use numbers to present approximate data. "
+            + "Provide a forecast of this person's impact on the global environment, considering their habits "
+            + "over the specified number of days:";
 }
