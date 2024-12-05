a5831af29d5362cc090da0dfb97a5a3d9b7668c7
Ivan Holotsvan <iholotsvan@gmail.com>
2024-12-05 14:36:01 +0200

    add functions to replace {times} to actual amount of times

--- service/src/main/java/greencity/service/NotificationServiceImpl.java
+++ service/src/main/java/greencity/service/NotificationServiceImpl.java
@@ -360,10 +360,15 @@ public class NotificationServiceImpl implements NotificationService {
         }
         String customMessage = notification.getCustomMessage() != null ? notification.getCustomMessage() : "";
         String secondMessage = notification.getSecondMessage() != null ? notification.getSecondMessage() : "";
+        int messagesCount = notification.getActionUsers().size();
+        String times = language.equals("ua")
+            ? resolveTimesInUkrainian(messagesCount)
+            : resolveTimesInEnglish(messagesCount);
         String body = bodyTemplate
             .replace("{user}", actionUserText)
             .replace("{message}", customMessage)
-            .replace("{secondMessage}", secondMessage);
+            .replace("{secondMessage}", secondMessage)
+            .replace("{times}", times);
 
         return ScheduledEmailMessage.builder()
             .email(notification.getTargetUser().getEmail())
@@ -378,5 +383,29 @@ public class NotificationServiceImpl implements NotificationService {
 
     private String createBaseLink(Notification notification) {
         return clientAddress + "/#/profile/" + notification.getTargetUser().getId() + "/notifications";
+    }
+
+    private String resolveTimesInEnglish(final int number) {
+        return switch (number) {
+            case 1 -> "";
+            case 2 -> "twice";
+            default -> number + " times";
+        };
+    }
+
+    private String resolveTimesInUkrainian(int number) {
+        number = Math.abs(number);
+        final int lastTwoDigits = number % 100;
+        final int lastDigit = number % 10;
+
+        if (lastTwoDigits >= 11 && lastTwoDigits <= 19) {
+            return number + " разів";
+        }
+
+        return switch (lastDigit) {
+            case 1 -> "";
+            case 2, 3, 4 -> number + " рази";
+            default -> number + " разів";
+        };
     }
 }
