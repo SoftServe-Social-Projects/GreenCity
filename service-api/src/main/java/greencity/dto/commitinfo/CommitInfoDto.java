560ecbc5dc23e455e92c48ea2f44c9cde13d5494
LazarenkoDmytro <lazarenkodmytro.work@gmail.com>
2024-12-19 17:29:41 +0200

    Replaced the polymorphism on DTOs with the single DTO

--- service-api/src/main/java/greencity/dto/commitinfo/CommitInfoDto.java
+++ service-api/src/main/java/greencity/dto/commitinfo/CommitInfoDto.java
@@ -1,7 +1,21 @@
 package greencity.dto.commitinfo;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
 /**
- * Base interface for commit information DTO.
+ * DTO for commit information response.
  */
-public interface CommitInfoDto {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class CommitInfoDto {
+    @NotNull
+    private String commitHash;
+    @NotNull
+    private String commitDate;
 }
