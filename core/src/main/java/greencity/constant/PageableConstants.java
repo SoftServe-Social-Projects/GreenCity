43e27fd0bb84ae45f36d7b76ee2c94c8e37aa991
whatIsLove <vlad.galyara@gmail.com>
2025-01-18 18:41:19 +0200

    fix: Resolve SonarCloud issues related to maintainability

--- core/src/main/java/greencity/constant/PageableConstants.java
+++ core/src/main/java/greencity/constant/PageableConstants.java
@@ -1,9 +1,12 @@
 package greencity.constant;
 
-public interface PageableConstants {
-    Integer DEFAULT_PAGE = 0;
-    Integer DEFAULT_PAGE_SIZE = 20;
-    String PAGE = "page";
-    String SIZE = "size";
-    Integer MAX_PAGE_SIZE = 100;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class PageableConstants {
+    public static final Integer DEFAULT_PAGE = 0;
+    public static final Integer DEFAULT_PAGE_SIZE = 20;
+    public static final String PAGE = "page";
+    public static final String SIZE = "size";
+    public static final Integer MAX_PAGE_SIZE = 100;
 }
--- core/src/test/java/greencity/config/CustomPageableHandlerMethodArgumentResolverTest.java
+++ core/src/test/java/greencity/config/CustomPageableHandlerMethodArgumentResolverTest.java
@@ -21,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class CustomPageableHandlerMethodArgumentResolverTest {
+class CustomPageableHandlerMethodArgumentResolverTest {
     private CustomPageableHandlerMethodArgumentResolver resolver;
 
     @Mock
