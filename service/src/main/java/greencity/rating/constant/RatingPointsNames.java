e5a42bfb208f2b29d9a3da5ac3eff502eb09e243
Warded120 <greenvania11@gmail.com>
2025-01-29 13:10:46 +0200

    moved String literals to constants

---
+++ service/src/main/java/greencity/rating/constant/RatingPointsNames.java
@@ -0,0 +1,6 @@
+package greencity.rating.constant;
+
+public class RatingPointsNames {
+    public static final String CREATE_NEWS = "CREATE_NEWS";
+    public static final String UNDO_CREATE_NEWS = "UNDO_CREATE_NEWS";
+}
