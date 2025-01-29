befcfd3a4c7d2c18b3212f24e8e2fdce2ccc8562
Hrenevych Ivan <121890324+Warded120@users.noreply.github.com>
2025-01-29 13:20:36 +0200

    Add @UtilityClass to resolve issue

--- service/src/main/java/greencity/rating/constant/RatingPointsNames.java
+++ service/src/main/java/greencity/rating/constant/RatingPointsNames.java
@@ -1,5 +1,8 @@
 package greencity.rating.constant;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class RatingPointsNames {
     public static final String CREATE_NEWS = "CREATE_NEWS";
     public static final String UNDO_CREATE_NEWS = "UNDO_CREATE_NEWS";
